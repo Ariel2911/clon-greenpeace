b3f151b417da5237128ee1597140fc2b3faae073
Ariel Barrios <ariel29111978@hotmail.com>
2022-08-18 10:35:30 -0300

    Crea el componente Header

---
+++ src/components/header/index.tsx
@@ -0,0 +1,16 @@
+const defaultProps ={
+  title: 'header',
+}
+export interface params {
+  title?: string;
+};
+
+const App = (params: params): JSX.Element => {
+  params = { ...defaultProps, ...params}
+  return (
+  <div>
+    <h1>{params.title}</h1>
+  </div>
+)};
+
+export default App;
